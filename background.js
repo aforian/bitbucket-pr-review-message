1c13d777acbb23351cfca0e974fb81a6108b1c20
Alex Ian <alexian853@gmail.com>
2023-03-29 15:32:52 +0800

    fix: not reset template after reinstall

--- background.js
+++ background.js
@@ -1,6 +1,10 @@
 const defaultReviewTemplate = `Hi Team! Change for {PR_HEADER}. Please review my pull request. Thank you! 🙏
 {PR_LINK} | {JIRA_CARD}`;
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ reviewTemplate: defaultReviewTemplate });
+chrome.runtime.onInstalled.addListener(async () => {
+  const { reviewTemplate } = await chrome.storage.sync.get(['reviewTemplate']);
+
+  if (!reviewTemplate) {
+    chrome.storage.sync.set({ reviewTemplate: defaultReviewTemplate });
+  }
 });
